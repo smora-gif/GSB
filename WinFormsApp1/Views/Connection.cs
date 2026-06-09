cd8d0064a3502243c81905aa4ceecaead9310d1b
smora-gif <sebastianmorac130@gmail.com>
2026-06-09 17:09:44 +0200

    Add files via upload

--- WinFormsApp1/Views/Connection.cs
+++ WinFormsApp1/Views/Connection.cs
@@ -1,20 +1,58 @@
+using System;
+using System.Windows.Forms;
+using WinFormsApp1.Modele;
+
 namespace WinFormsApp1
 {
     public partial class Connection : Form
     {
+        // On déclare notre couche d'accès aux données
+        private MedecinRepository _medecinRepository;
+
         public Connection()
         {
             InitializeComponent();
+            // On l'initialise au chargement du formulaire
+            _medecinRepository = new MedecinRepository();
         }
 
-        private void btnConnection_Click(object sender, EventArgs e)
+        private void btnConnection_Click(object sender, EventArgs e) //boutton connection
         {
-            string Username = txtboxUtilisateur.Text;
+            string Username = txtboxUtilisateur.Text.Trim();
             string Password = txtboxMdp.Text;
-            MessageBox.Show("User :" + Username + " " + "PassWord :" + " " + Password);
 
-            Form cPageMedecinForm = new Page_Medecin();
-            cPageMedecinForm.Show();
+            // Validation de base
+            if (string.IsNullOrEmpty(Username) || string.IsNullOrEmpty(Password))
+            {
+                MessageBox.Show("Veuillez remplir tous les champs.", "Champs obligatoires", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                // Appel de la couche d'accès aux données
+                Medecin medecinConnecte = _medecinRepository.Connexion(Username, Password);
+
+                if (medecinConnecte != null)
+                {
+                    MessageBox.Show($"Bienvenue Docteur {medecinConnecte.Prenom} !", "Connexion réussie", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    // Redirection vers Page_Medecin 
+                    Form cPageMedecinForm = new Page_Medecin();
+                    cPageMedecinForm.Show();
+
+                    // On cache la page de connexion
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Identifiants incorrects. Veuillez réessayer.", "Échec de connexion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erreur Système", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void linkInscription_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
---
+++ WinFormsApp1/Views/MedecinRepository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using WinFormsApp1.Modele;
+
+namespace WinFormsApp1
+{
+    public class MedecinRepository
+    {
+        private string connectionString = "Server=localhost;Port=3306;Database=gsb_ordonnances;Uid=gsb;Pwd=gsbpass;";
+
+        public Medecin Connexion(string rpps, string password)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    // La requête sur la table 'medecin' en minuscules
+                    string query = "SELECT * FROM MEDECIN WHERE numeroRPPS = @rpps AND motDePasse = @password";
+
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@rpps", rpps);
+                        cmd.Parameters.AddWithValue("@password", password);
+
+                        using (MySqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                // Récupération des chaînes de caractères (Sécurisées)
+                                string numRpps = reader["numeroRPPS"] != DBNull.Value ? reader["numeroRPPS"].ToString() : "";
+                                string prenom = reader["prenom"] != DBNull.Value ? reader["prenom"].ToString() : "";
+                                string mail = reader["email"] != DBNull.Value ? reader["email"].ToString() : "";
+                                string mdp = reader["motDePasse"] != DBNull.Value ? reader["motDePasse"].ToString() : "";
+                                string spe = reader["specialite"] != DBNull.Value ? reader["specialite"].ToString() : "";
+
+                                // CORRECTION DE L'ERREUR : On vérifie si la colonne existe ou on utilise un "try/catch" local
+                                DateTime dateNais = DateTime.Now;
+                                try
+                                {
+                                    // Essaie d'abord de lire 'date_naissance'
+                                    dateNais = reader["date_naissance"] != DBNull.Value ? Convert.ToDateTime(reader["date_naissance"]) : DateTime.Now;
+                                }
+                                catch
+                                {
+                                    try
+                                    {
+                                        // Si 'date_naissance' n'existe pas, essaie 'dateNaissance'
+                                        dateNais = reader["dateNaissance"] != DBNull.Value ? Convert.ToDateTime(reader["dateNaissance"]) : DateTime.Now;
+                                    }
+                                    catch
+                                    {
+                                        // Si ça plante encore, on garde DateTime.Now par défaut sans bloquer l'application
+                                        dateNais = DateTime.Now;
+                                    }
+                                }
+
+                                // Renvoie l'objet Médecin vers ta Page_Medecin
+                                return new Medecin(numRpps, prenom, mail, mdp, mdp, spe, dateNais);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Erreur de connexion à la base de données Docker : " + ex.Message);
+                }
+            }
+            return null;
+        }
+    }
+}
